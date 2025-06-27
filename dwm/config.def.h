0a00168750b318c7b5ef87f94a039d7c987f5f2f
marcocappelloni <marco.cappelloni@gmail.com>
2025-06-27 19:49:36 +0100

    Added floating setting for pomatez window

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -42,6 +42,7 @@ static const Rule rules[] = {
 	{ "Gimp",         NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",      NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "pavucontrol",  NULL,       NULL,       0,            1,           -1 },
+	{ "pomatez",      NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
